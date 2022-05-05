4c59afc90c271025f25db79dd376e865ff55ffa6
Ipmake123 <niko.denis727@gmail.com>
2022-05-05 22:38:39 +0200

    added a redirect.add command

--- tools/commands/redirects.add.js
+++ tools/commands/redirects.add.js
@@ -6,11 +6,13 @@ module.exports = {
     description: 'Add a redirect.',
     async execute(args, cache) {
         log('system', 'Trying to add redirect...', 'console')
-        if(!args[0] || !args[1] || args[2]) return log('syntax', 'SYNTAX ERROR: redirects.add [host] [from] [to]', 'console'), true
+        if(!args[0] || !args[1] || !args[2]) return log('syntax', 'SYNTAX ERROR: redirects.add [host] [from] [to]', 'console'), true
 
         var redirects = JSON.parse(fs.readFileSync('./router/routes.json', 'utf8'));
         redirects.redirects.push({ host: args[0], dir: args[1], target: args[2]});
-        fs.writeFileSync('./router/routes.json', JSON.stringify(redirects, null, 2));
+        fs.writeFileSync('./router/routes.json', JSON.stringify(redirects));
+
+        cache.init()
 
         log("sysOK", null, 'ADD REDIRECT')
         return true
